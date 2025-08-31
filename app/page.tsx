602783214134c6746886cb4ada8225c1c773882a
Brajesh Singh <brajeshsingh2911@gmail.com>
2025-09-01 02:44:01 +0530

    Implementation Table Ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,54 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/Cta'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-     <h1>Home page</h1>
-    </div>
+    <main>
+      <h1 >Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+        id='123'
+        name= 'Neura the Brainy Explorer'
+        topic=' Neural Network of the Brain'
+        subject='Science'
+        duration={45}
+        color = '#ffda6e'
+
+        />
+       
+ <CompanionCard 
+        id='456'
+        name= 'Countsy the Number Wizard'
+        topic='Derivatives & Integrals'
+        subject='Maths'
+        duration={30}
+        color = '#e5d0ff'
+
+        />
+         <CompanionCard 
+        id='789'
+        name= 'Verba the Vocabulary Builder'
+        topic='  English Literature '
+        subject='Language'
+        duration={30}
+        color = '#BDE7FF'
+
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList 
+        title= 'Recently completed lessons'
+        companions={recentSessions}
+        classNames = "w-2/3 max-lg:w-full"
+
+        />
+        <Cta />
+      </section>
+    </main>
   )
 }
 
